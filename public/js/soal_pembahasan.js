20dfe9a424185a1595f07413d0bb0842334aaed7
Cloudias Pradana <danapradana30@gmail.com>
2021-11-15 15:00:11 +0700

    fix pembahasan button and style

--- public/js/soal_pembahasan.js
+++ public/js/soal_pembahasan.js
@@ -1,14 +1,14 @@
 const pembahasan = document.querySelectorAll(".pembahasan");
-pembahasan.forEach(el=>{
-    // Make default display for pembahasan hidden
-    el.classList.add('hidden');
-    const childs = el.children[1].children;
+for (let i = 0; i < pembahasan.length; i++) {
+    pembahasan[i].classList.add('hidden');
+    const childs = pembahasan[i].querySelectorAll('*');
     for (let i = 0; i < childs.length; i++) {
         // give background color and padding for children in pembahasan
         childs[i].style.backgroundColor = '#EEF4ED';
-        childs[i].style.padding = '25px';
+        // childs[i].style.padding = '25px';
     }
-});
+    
+}
 
 // show and hide pembahasan with button
 function showPembahasan(obj={}){
